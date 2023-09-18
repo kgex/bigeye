8d55195c3ce45fce6bc9af03c1a380f69d9dbf0b
tarunkumark <tkksctwo@gmail.com>
2023-09-18 20:48:31 +0530

    QRCode Gen, ticket raising, oauth,

--- tickets/urls.py
+++ tickets/urls.py
@@ -4,11 +4,9 @@ from . import views
 
 urlpatterns = [
     # ex: /tickets/
-    path("", views.index, name="index"),
-    # ex: /tickets/5/
-    path("<int:question_id>/", views.detail, name="detail"),
-    # ex: /tickets/5/results/
-    path("<int:question_id>/results/", views.results, name="results"),
-    # ex: /tickets/5/vote/
-    path("<int:question_id>/vote/", views.vote, name="vote"),
+    path("<int:wr_name>/", views.ticket, name="index"),
+    # path("login/", views.login, name="login"),
+    path("login/", views.login, name="login"),
+    path("", views.home, name="home"),
+    path("logout/", views.logout, name="logout"),
 ]
--- tickets/views.py
+++ tickets/views.py
@@ -1,26 +1,49 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.http import HttpResponse
-from .models import Question
+from .models import Washroom, Ticket, CustomUser, Role
 from django.template import loader
-
+from django.contrib.auth.decorators import login_required
+from allauth.account.views import LoginView, LogoutView, SignupView
+from allauth.socialaccount.views import ConnectionsView
 # Create your views here.
+from allauth.account.views import LoginView
 
-def index(request):
-    latest_question_list = Question.objects.order_by("-pub_date")[:5]
-    template = loader.get_template("tickets/index.html")
-    context = {
-        "latest_question_list": latest_question_list,
-    }
+def login(request):
+    # Your custom logic here, if needed
+    return LoginView.as_view(template_name='polls/login.html')(request)
+@login_required(login_url='/tickets/login/')
+def ticket(request, wr_name):
+    if request.method == 'POST':
+        wr = Washroom.objects.get(name=wr_name)
+        title = request.POST.get('title')
+        print(title)
+        print(request.POST.get('title_text'))
+        title_text = request.POST.get('title_text')
+        user = CustomUser.objects.get_or_create(email=request.user.email, username=request.user.email)[0]
+        ticket = Ticket(title=title, ticket_text=title_text, washroom=wr, user=user)
+        ticket.save()
+        return redirect('home')
+    wr = Washroom.objects.get(name=wr_name)
+    context = {"wr_name" : wr.name, "wr_location" : wr.location, "wr_condition" : wr.condition}
+    template = loader.get_template('polls/ticket.html') 
     return HttpResponse(template.render(context, request))
 
-def detail(request, question_id):
-    return HttpResponse("You're looking at question %s." % question_id)
+@login_required(login_url='/tickets/login/')
+def home(request):
+    # print(request.user.email_address)
+    # print(request.user.email)
+    for obj in CustomUser.objects.all():
+        print(obj.email)
+    try:
+        user = CustomUser.objects.get_or_create(email=request.user.email, username=request.user.email)[0]
+        user_tickets = Ticket.objects.filter(user=user)
+    except:
+        user_tickets = []
+    context = {"user_tickets" : user_tickets}
+    template = loader.get_template('polls/home.html')
+    return HttpResponse(template.render(context, request))
 
+@login_required(login_url='/tickets/login/')
+def logout(request):
+    return LogoutView.as_view()(request)
 
-def results(request, question_id):
-    response = "You're looking at the results of question %s."
-    return HttpResponse(response % question_id)
-
-
-def vote(request, question_id):
-    return HttpResponse("You're voting on question %s." % question_id)
